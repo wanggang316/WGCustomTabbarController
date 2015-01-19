c5886e60366fdf930478ce2bf5178db7498014a5
wanggang <wgang316@163.com>
2015-01-19 09:28:58 +0800

    Initial Commit

---
+++ WGCustomTabbarController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WGCustomTabbarController
+//
+//  Created by 王刚 on 15/1/19.
+//  Copyright (c) 2015年 ccidnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WGCustomTabbarController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WGCustomTabbarController
+//
+//  Created by 王刚 on 15/1/19.
+//  Copyright (c) 2015年 ccidnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
